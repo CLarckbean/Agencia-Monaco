ff6d6206cb53a8469f070abb691dbb89c78a7375
CLarckbean <ecolelito@gmail.com>
2025-12-23 16:02:14 +0100

    Logo Topo

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,9 +17,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-            <span className="text-2xl font-bold bg-gradient-to-r from-purple-400 to-purple-600 bg-clip-text text-transparent">
-              MONACO
-            </span>
+              <img src="Logo1" alt="" />
           </div>
 
           <div className="hidden md:block">
