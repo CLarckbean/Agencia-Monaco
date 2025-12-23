1822eb27730f4af3b2181596a597773689617f25
CLarckbean <ecolelito@gmail.com>
2025-12-23 16:16:47 +0100

    Logo tentativa 03

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -17,7 +17,7 @@ export default function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16">
           <div className="flex-shrink-0">
-              <img src="/Logo1.png" alt="Logo" className="h-10 w-auto" />
+              <img src="/Logo1.png" alt="Logo" className="h-10 w-16" />
           </div>
 
           <div className="hidden md:block">
